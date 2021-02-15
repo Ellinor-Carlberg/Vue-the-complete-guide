82c0fbe521bf1b831a72a6002497a2c55dabf5f3
Ellinor Carlberg <ellinor.carlberg@medieinstitutet.se>
2021-02-15 10:26:19 +0100

    build a small test app with vanilla JS

---
+++ gs-01-starting-project/app.js
@@ -0,0 +1,15 @@
+
+const buttonEl = document.querySelector('button');
+const inputEl = document.querySelector('input');
+const listEl = document.querySelector('ul');
+const listItem = document.createElement('li');
+ 
+function addGoal() {
+    const enteredValue = inputEl.value;
+    const listItemEl = document.createElement('li');
+    listItemEl.textContent = enteredValue;
+    listEl.appendChild(listItemEl);
+    inputEl.value = '';
+}
+
+buttonEl.addEventListener('click', addGoal);
