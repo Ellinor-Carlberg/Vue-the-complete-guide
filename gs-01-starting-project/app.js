3c35d791655cf9d5354c1cb40cf70f557b6f8f2f
Ellinor Carlberg <ellinor.carlberg@medieinstitutet.se>
2021-02-15 12:14:39 +0100

    same application but with vue 3 instead of JS

--- gs-01-starting-project/app.js
+++ gs-01-starting-project/app.js
@@ -1,9 +1,11 @@
 
+/* 
+//get access to button, input, ul element
 const buttonEl = document.querySelector('button');
 const inputEl = document.querySelector('input');
 const listEl = document.querySelector('ul');
-const listItem = document.createElement('li');
- 
+
+//button click function -create a list item with the value from input
 function addGoal() {
     const enteredValue = inputEl.value;
     const listItemEl = document.createElement('li');
@@ -11,5 +13,23 @@ function addGoal() {
     listEl.appendChild(listItemEl);
     inputEl.value = '';
 }
+/click event -button click function
+buttonEl.addEventListener('click', addGoal); 
+*/
 
-buttonEl.addEventListener('click', addGoal);
+Vue.createApp({
+    data() {
+        return {
+            goals: [],
+            enteredValue: ''
+        };
+    },
+    methods: {
+        addGoal() {
+            this.goals.push(this.enteredValue);
+            this.enteredValue = ''
+        }
+    }
+}).mount('#app');
+
+
