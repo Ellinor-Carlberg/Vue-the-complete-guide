8bc07a8d8ff24e07e746cf217ee9fe90fa0ffd99
Ellinor Carlberg <ellinor.carlberg@medieinstitutet.se>
2021-02-16 17:28:36 +0100

    events

--- 02.Basics-01-starting-code/03.basics-events-starting-code/app.js
+++ 02.Basics-01-starting-code/03.basics-events-starting-code/app.js
@@ -2,8 +2,24 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      confirmedName: '',
+      name: ''
     };
+  },
+  methods: {
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    remove(num) {
+      this.counter = this.counter - num;
+    },
+    setName(event, lastName) {
+      this.name = event.target.value + ' ' +lastName;
+    },
+    confirmInput() {
+      this.confirmedName = this.name;
+    }
   },
 });
 
-app.mount('#events');
+app.mount("#events");
